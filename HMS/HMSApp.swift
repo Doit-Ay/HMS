8b5841e475560a9c7537538858fd8326c6b87d03
Aditya yadav <aditya112004ahir@gmail.com>
2026-03-07 11:00:03 +0530

    Initial Commit

---
+++ HMS/HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMSApp.swift
+//  HMS
+//
+//  Created by admin99 on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HMSTests/HMSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HMSTests.swift
+//  HMSTests
+//
+//  Created by admin99 on 07/03/26.
+//
+
+import Testing
+@testable import HMS
+
+struct HMSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
